0c198ab88deceae931021decb31254f1cafd1f15
v_yameiwu <v_yameiwu@tencent.com>
2020-12-05 17:48:11 +0800

    feat: update

--- tcp-chat/index.js
+++ tcp-chat/index.js
@@ -2,11 +2,20 @@ var net = require('net');
 
 var colorStart = '\033[96m';
 var colorEnd = '\033[39m';
+
+// 记录当前聊天人数
+var count = 0;
 // 创建一个tcp服务
 var server = net.createServer(function (conn) {
   // handle connection
-  console.log(colorStart + 'new connection!' + colorEnd);
-  // console.log('conn', conn);
+  console.log('new connnection!');
+  
+  conn.write(
+      '\n > Welcome to \033[92m node-chat!' + colorEnd
+    + '\n > ' + count + ' other people are connected at this time.'
+    + '\n > please write your name and press enter'
+  );
+  count++;
 });
 
 // 监听
